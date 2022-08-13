f09078e22e63a7afac86c8e5d1aa4b7f71eb1812
Bhenri <brandon.reis01@etec.sp.gov.br>
2022-08-13 01:42:07 +0000

    calc

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ITANHAÉM, SÓ MLK LOKOOOOOOO...</Text>
-      <StatusBar style="auto" />
-    </View>
+		<View style={styles.bloco}>
+		  <Text style={styles.titulo}>Calculadora simples</Text> 
+		</View>
+    
+	  	<View style={styles.bloco}>
+		  <Text>Valor 1:</Text>
+			  <TextInput style={styles.input}></TextInput>
+		  </View>
+		  <View style={styles.bloco}>
+		  <Text>Valor 2:</Text>
+			  <TextInput style={styles.input}></TextInput>
+		  </View>
+		  <View style={styles.bloco}>
+		  <TouchableOpacity style={styles.botao}>
+			  <Text style={styles.textoBotao}>SOMAR</Text>
+			  </TouchableOpacity>
+		  </View>
+	</View>				
+	 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	  backgroundColor: 'cyan'
   },
+	
+	bloco:{
+		marginTop:20,
+		width:'80%',
+		marginLeft:'10%'
+	},
+	titulo:{
+		fontSize:30,
+		textAlign:'center'
+	},
+	input:{
+		borderWidth:2,
+		fontSize:20,
+		borderRadius:10
+	},
+	botao:{
+		backgroundColor:'blue',
+		borderRadius:10
+	},
+	textoBotao:{
+		color: '#fff',
+		textAlign:'center',
+		fontSize:20
+	}
 });
