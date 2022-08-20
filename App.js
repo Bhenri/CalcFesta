502bdcfde5b47bebc3f05f602351cfc6ce86bd6f
Bhenri <brandon.reis01@etec.sp.gov.br>
2022-08-20 01:44:20 +0000

    calculando

--- App.js
+++ App.js
@@ -1,25 +1,49 @@
-import React from 'expo-status-bar';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+	const [valor1, setValor1] = useState("20");
+	const [valor2, setValor2] = useState("3");
+	const [resultado,setResultado] =useState("");
+	function somar (){
+		let r= parseFloat(valor1) + parseFloat(valor2);
+		setResultado(r);
+		
+	}
   return (
-    <View style={styles.container}>
+    <View style={styles.	  ntainer}>
 		<View style={styles.bloco}>
 		  <Text style={styles.titulo}>Calculadora simples</Text> 
 		</View>
     
 	  	<View style={styles.bloco}>
 		  <Text>Valor 1:</Text>
-			  <TextInput style={styles.input}></TextInput>
+			  <TextInput style={styles.input}
+				  value={valor1}
+				  onChangeText = {(valor)=>setValor1(valor)}
+				  keyboardType="numeric"
+				/>
 		  </View>
 		  <View style={styles.bloco}>
 		  <Text>Valor 2:</Text>
-			  <TextInput style={styles.input}></TextInput>
+			  <TextInput style={styles.input}
+				  value={valor2}
+				  onChangeText = {(valor)=>setValor2(valor)}
+				  keyboardType="numeric"
+				  />
 		  </View>
 		  <View style={styles.bloco}>
-		  <TouchableOpacity style={styles.botao}>
+		  <TouchableOpacity 
+			  style={styles.botao}
+			  onPress={somar}
+			  >
 			  <Text style={styles.textoBotao}>SOMAR</Text>
 			  </TouchableOpacity>
+		  </View>
+		  <View style={styles.bloco}>
+		  <Text style={styles.titulo}>
+			  RESULTADO: {resultado}
+			  </Text>
 		  </View>
 	</View>				
 	 
@@ -29,9 +53,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-	  backgroundColor: 'cyan'
+	  backgroundColor: '#FF5733'
   },
-	
 	bloco:{
 		marginTop:20,
 		width:'80%',
